15fa08a534bdb3d15591571fb96059b8685faff6
dusanstanojeviccs <dusan.stanojevic.cs@gmail.com>
2021-05-02 10:39:38 -0500

    Rule fix

--- tests/parser/update.parser.test.js
+++ tests/parser/update.parser.test.js
@@ -3,50 +3,255 @@ let lightningSql = require("../../dist/lightning-sql").lightningSql;
 let query, result;
 
 query = "UPDATE users SET name = 'demo' WHERE id = 200";
-result = [
-    new lightningSql.Token(1,"UPDATE","UPDATE",-1,6),
-    new lightningSql.Token(1,"ID","users",7,12),
-    new lightningSql.Token(1,"SET","SET",13,16),
-    new lightningSql.Token(1,"ID","name",17,21),
-    new lightningSql.Token(1,"EQ","=",23,23),
-    new lightningSql.Token(1,"STRING","demo",24,30),
-    new lightningSql.Token(1,"WHERE","WHERE",31,36),
-    new lightningSql.Token(1,"ID","id",37,39),
-    new lightningSql.Token(1,"EQ","=",41,41),
-    new lightningSql.Token(1,"NUMBER","200",-1,-1),
-    new lightningSql.Token(1,"END-OF-FILE","",46,46),
-];
+result = {
+    "type": "PROGRAM",
+    "children": [
+        {
+            "type": "UPDATE_STMT",
+            "children": [
+                {
+                    "type": "UPDATE",
+                    "value": "UPDATE",
+                    "children": []
+                },
+                {
+                    "type": "TABLES",
+                    "children": [
+                        {
+                            "type": "COMPLEX_ID",
+                            "children": [
+                                {
+                                    "type": "ID",
+                                    "value": "users",
+                                    "children": []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "type": "SET",
+                    "value": "SET",
+                    "children": []
+                },
+                {
+                    "type": "ASSIGNMENT_LIST",
+                    "children": [
+                        {
+                            "type": "ASSIGNMENT_STMT",
+                            "children": [
+                                {
+                                    "type": "COMPLEX_ID",
+                                    "children": [
+                                        {
+                                            "type": "ID",
+                                            "value": "name",
+                                            "children": []
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "EQ",
+                                    "value": "=",
+                                    "children": []
+                                },
+                                {
+                                    "type": "VALUE",
+                                    "children": [
+                                        {
+                                            "type": "STRING",
+                                            "value": "demo",
+                                            "children": []
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "type": "WHERE",
+                    "value": "WHERE",
+                    "children": []
+                },
+                {
+                    "type": "EXPRESSION",
+                    "children": [
+                        {
+                            "type": "COMPARRISON",
+                            "children": [
+                                {
+                                    "type": "VALUE",
+                                    "children": [
+                                        {
+                                            "type": "COMPLEX_ID",
+                                            "children": [
+                                                {
+                                                    "type": "ID",
+                                                    "value": "id",
+                                                    "children": []
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "COMPARRISON_CONNECTIVE",
+                                    "children": [
+                                        {
+                                            "type": "EQ",
+                                            "value": "=",
+                                            "children": []
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "VALUE",
+                                    "children": [
+                                        {
+                                            "type": "NUMBER",
+                                            "value": "200",
+                                            "children": []
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
 test(query, () => {
 	let lxr = new lightningSql.Lexer(query);
-	expect(lxr.readAllTokens()).toStrictEqual(result);
+	let parser = new lightningSql.Parser(lxr);
+	expect(JSON.stringify(parser.parseProgram())).toBe(JSON.stringify(result));
 });
+
+
+
+
+
 
 
 query = "UPDATE users SET name = 'demo', email='demo@email.com' WHERE id IN (SELECT user_id FROM demo_users)";
-result = [
-    new lightningSql.Token(1,"UPDATE","UPDATE",-1,6),
-    new lightningSql.Token(1,"ID","users",7,12),
-    new lightningSql.Token(1,"SET","SET",13,16),
-    new lightningSql.Token(1,"ID","name",17,21),
-    new lightningSql.Token(1,"EQ","=",23,23),
-    new lightningSql.Token(1,"STRING","demo",24,30),
-    new lightningSql.Token(1,"COMMA",",",31,31),
-    new lightningSql.Token(1,"ID","email",32,37),
-    new lightningSql.Token(1,"EQ","=",38,38),
-    new lightningSql.Token(1,"STRING","demo@email.com",38,54),
-    new lightningSql.Token(1,"WHERE","WHERE",55,60),
-    new lightningSql.Token(1,"ID","id",61,63),
-    new lightningSql.Token(1,"IN","IN",64,66),
-    new lightningSql.Token(1,"LBRACE","(",68,68),
-    new lightningSql.Token(1,"SELECT","SELECT",68,74),
-    new lightningSql.Token(1,"ID","user_id",75,82),
-    new lightningSql.Token(1,"FROM","FROM",83,87),
-    new lightningSql.Token(1,"ID","demo_users",88,98),
-    new lightningSql.Token(1,"RBRACE",")",99,99),
-    new lightningSql.Token(1,"END-OF-FILE","",100,100),
-];
+result = {
+    "type": "PROGRAM",
+    "children": [
+        {
+            "type": "UPDATE_STMT",
+            "children": [
+                {
+                    "type": "UPDATE",
+                    "value": "UPDATE",
+                    "children": []
+                },
+                {
+                    "type": "TABLES",
+                    "children": [
+                        {
+                            "type": "COMPLEX_ID",
+                            "children": [
+                                {
+                                    "type": "ID",
+                                    "value": "users",
+                                    "children": []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "type": "SET",
+                    "value": "SET",
+                    "children": []
+                },
+                {
+                    "type": "ASSIGNMENT_LIST",
+                    "children": [
+                        {
+                            "type": "ASSIGNMENT_STMT",
+                            "children": [
+                                {
+                                    "type": "COMPLEX_ID",
+                                    "children": [
+                                        {
+                                            "type": "ID",
+                                            "value": "name",
+                                            "children": []
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "EQ",
+                                    "value": "=",
+                                    "children": []
+                                },
+                                {
+                                    "type": "VALUE",
+                                    "children": [
+                                        {
+                                            "type": "STRING",
+                                            "value": "demo",
+                                            "children": []
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            "type": "COMMA",
+                            "value": ",",
+                            "children": []
+                        },
+                        {
+                            "type": "ASSIGNMENT_LIST",
+                            "children": [
+                                {
+                                    "type": "ASSIGNMENT_STMT",
+                                    "children": [
+                                        {
+                                            "type": "COMPLEX_ID",
+                                            "children": [
+                                                {
+                                                    "type": "ID",
+                                                    "value": "email",
+                                                    "children": []
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            "type": "EQ",
+                                            "value": "=",
+                                            "children": []
+                                        },
+                                        {
+                                            "type": "VALUE",
+                                            "children": [
+                                                {
+                                                    "type": "STRING",
+                                                    "value": "demo@email.com",
+                                                    "children": []
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
 test(query, () => {
 	let lxr = new lightningSql.Lexer(query);
-	expect(lxr.readAllTokens()).toStrictEqual(result);
+	let parser = new lightningSql.Parser(lxr);
+	expect(JSON.stringify(parser.parseProgram())).toBe(JSON.stringify(result));
 });
+
+
+
 
